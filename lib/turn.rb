aa86e38b104878b4c8442743c9a964b2f7cb25eb
Varshini Ananta <sananta@ucdavis.edu>
2019-05-26 06:03:19 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,4 +34,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move()
+    move(board, index)
+    display_board(board)
+  else
+    puts "Please enter 1-9:"
